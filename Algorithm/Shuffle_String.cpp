98343cef0e50d9056d3afd0b54f03e74eb47a8d6
ql <qiulin@ihep.ac.cn>
2020-08-03 21:00:59 +0800

    nil

--- Algorithm/Shuffle_String.cpp
+++ Algorithm/Shuffle_String.cpp
@@ -1,11 +1,15 @@
 #include <string>
-#include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
-      //Using Group Method
+      string rstr(s);
+      for(int i=0;i<int(s.size());i++){
+	rstr[indices[i]]=s[i];
+      }
+      return rstr;
     }
 };
