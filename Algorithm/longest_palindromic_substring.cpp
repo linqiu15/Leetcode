1e0f85b2a40830a52d0a75d3ec0ce4d3875ba6c0
ql <qiulin@ihep.ac.cn>
2021-01-17 22:19:14 +0800

    add a time-exceeded submission

--- Algorithm/longest_palindromic_substring.cpp
+++ Algorithm/longest_palindromic_substring.cpp
@@ -2,40 +2,48 @@
 #include <string>
 using namespace std;
 
+// Time limit exceeded
+// class Solution {
+// public:
+//   string longestPalindrome(string s) {
+//     int len = s.length();
+//     string rstr = "";
+
+//     // brute-force searching
+//     for (int i = 0; i < len; i++) {
+//       int j = len - 1;
+//       while (j >= i) {
+//         for (; j >= i && s[i] != s[j]; j--) {
+//         }
+//         // if there is no element similar to s[i]
+//         if (i == j) {
+//           rstr = rstr.length()<=1?s.substr(i,1):rstr;
+//           break;
+//         }
+//         int k = 1;
+//         for (; k < (j - i + 1) / 2 && s[i + k] == s[j - k]; k++) {
+//         }
+//         if (k >= (j - i + 1) / 2 && rstr.length() <= (j - i + 1)) {
+//           rstr = s.substr(i, j - i + 1);
+//           break;
+//         }
+//         j--;
+//       }
+//     }
+
+//     return rstr;
+//   }
+// };
+
 class Solution {
 public:
-  string longestPalindrome(string s) {
-    int len = s.length();
-    string rstr = "";
-
-    // brute-force searching
-    for (int i = 0; i < len; i++) {
-      for (int j = len - 1; j >= i && s[i] == s[j]; j--) {
-        // if (s[i] == s[j]) {
-
-        //   int k = 1;
-        //   while (k < (j - i + 1) / 2 && s[i + k] == s[j - k]) {
-        //     k++;
-        //   }
-
-        //   if (k >= (j - i + 1) / 2 && rstr.length() <= (j - i + 1)) {
-        //     rstr = s.substr(i, j - i + 1);
-        //     break;
-        //   }
-        // }
-        int k = 1;
-
-        while (k < (j - i + 1) / 2 && s[i + k] == s[j - k]) {
-          k++;
-        }
-
-        if (k >= (j - i + 1) / 2 && rstr.length() <= (j - i + 1)) {
-          rstr = s.substr(i, j - i + 1);
-          break;
-        }
-      }
+    string longestPalindrome(string s) {
+      
     }
-
-    return rstr;
-  }
 };
+
+int main(void){
+  Solution *sol=new Solution();
+  cout<<sol->longestPalindrome("babad")<<endl;
+  return 0;
+}
